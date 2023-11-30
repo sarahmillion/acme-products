dabcc2520b4ccb5ff9a133137be1050c499a4e25
Sarah Million <sarahmillion@Sarahs-Air.home>
2023-11-30 10:41:08 -0600

    added content

--- index.js
+++ index.js
@@ -5,24 +5,48 @@ const ul = document.querySelector('ul');
 
 const products = [
 {name: 'foo', color: 'red', cost:1},
-{name: 'blue', color: 'blue', cost:3}
+{name: 'bar', color: 'blue', cost:3}
 ];
 
-spanCount.innerHTML = products.length;
+const names = [ 'foo', 'bar', 'bazz', 'quq'];
+const color = ['red', 'blue', 'green', 'yellow'];
+
+function render(){
+ spanCount.innerHTML = products.length;
 
 let sum = 0;
-products.forEach((products)=> {
-sum = sum + products.cost;
+ products.forEach((products)=> {
+ sum = sum + products.cost;
 });
 
-const average = sum / products.length;
-spanAverageCost.innerHTML = average; 
+ const average = sum / products.length;
+ spanAverageCost.innerHTML = average; 
 
-const html = products.map ((product)=> {
+ const html = products.map ((product)=> {
  return`
- <li>${product.name} is ${product.color} and cost ${product.cost}
- </li>
+  <li>${product.name} is ${product.color} and cost ${product.cost.toFixed(2)}
+  </li>
 `;
 }).join('');
 
 ul.innerHTML = html;
+}
+render();
+
+setInterval(()=> {
+    const nameIndex = Math.floor(Math.random()*names.length);
+    const name = names[nameIndex];
+    const colorIndex = Math.floor(Math.random()*names.length);
+    const color = color[colorIndex];
+    const cost = Math.ceil(Math.random()*10);
+    const randomProduct = {
+        name: name,
+        color: color, 
+        cost:cost
+    };
+products.push(randomProduct);
+render();   
+if(products.length === 15){
+clearInterval(interval);
+}
+}, 1000)
